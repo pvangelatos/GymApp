7369479a8d711228d16987c4393a7e8099814cee
pvangelatos <p.vangelatos23@hotmail.com>
2026-04-19 12:10:43 +0300

    Add project files.

---
+++ GymApp/Program.cs
@@ -0,0 +1,38 @@
+using GymApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration
+                    .GetConnectionString("DefaultConnection")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapRazorPages()
+               .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
